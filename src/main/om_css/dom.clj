2de107cd768a7eed9abb86d42fd637eec6cbcd3c
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-06 15:21:08 +0100

    bring in dom.clj & dom.cljs

---
+++ src/main/om_css/dom.clj
@@ -0,0 +1,32 @@
+(ns om-css.dom
+  (:refer-clojure :exclude [map meta time])
+  (:require [om.dom :as dom]))
+
+;;; defcomponent
+
+(defmacro defcomponent
+  [component-name [props children] & body]
+  "Example usage:
+  (defcomponent foo
+    [props children]
+    (dom/div {:class \"foo\"}
+             children))
+
+  (foo (dom/a {:href \"http://google.com\"}))
+  "
+  (list 'defn component-name '[& params]
+        (concat (list 'let [[props (or children '_)] `(parse-params ~'params)])
+                body)))
+
+;;; generate all form tags
+
+(def form-tags '[input textarea option])
+
+(def all-tags (concat dom/tags form-tags))
+
+(defmacro gen-tag-fns
+  []
+  `(do
+     ~@(clojure.core/map (fn [tag] `(defn ~tag [& ~'params]
+                                      (apply render-elem ~(symbol "om.dom" (name tag)) ~'params)))
+                         all-tags)))
---
+++ src/main/om_css/dom.cljs
@@ -0,0 +1,90 @@
+(ns om-css.dom
+  (:refer-clojure :exclude [map meta time mask])
+  (:require-macros [om-css.dom :refer [gen-tag-fns format-class-name]])
+  (:require [om.dom :as dom]
+            [clojure.string :as string]))
+
+;;; lifted from https://github.com/plumatic/om-tools/blob/master/src/om_tools/dom.cljx
+
+(defn camel-case
+  "Converts kebab-case to camelCase"
+  [s]
+  (string/replace s #"-(\w)" (comp string/upper-case second)))
+
+(defn- opt-key-case
+  "Converts attributes that are kebab-case and should be camelCase"
+  [attr]
+  (if (or (< (count attr) 5)
+          (case (subs attr 0 5) ("data-" "aria-") true false))
+    attr
+    (camel-case attr)))
+
+(defn- opt-key-alias
+  "Converts aliased attributes"
+  [opt]
+  (case opt
+    :class :className
+    :for :htmlFor
+    opt))
+
+(defn format-opt-key
+  "Returns potentially formatted name for DOM element attribute.
+   Converts kebab-case to camelCase."
+  [opt-key]
+  (-> opt-key
+      opt-key-alias
+      name
+      opt-key-case
+      keyword))
+
+(declare format-opts)
+
+(defn format-opt-val
+  "Returns potentially modified value for DOM element attribute.
+   Recursively formats map values (ie :style attribute)"
+  [opt-val]
+  (cond
+    (map? opt-val)
+    (format-opts opt-val)
+    :else
+    opt-val))
+
+(defn format-opts
+  "Returns JavaScript object for React DOM attributes from opts map"
+  [opts]
+  (if (map? opts)
+    (->> opts
+         (clojure.core/map
+          (fn [[k v]] [(format-opt-key k) (format-opt-val v)]))
+         (into {})
+         clj->js)
+    opts))
+
+(defn parse-params
+  [params]
+  (update
+   (if (map? (first params))
+     [(first params) (rest params)]
+     [nil params])
+   1 flatten))
+
+(defn render-elem
+  [render & params]
+  (let [[attrs children] (parse-params params)]
+    (apply render (format-opts attrs) children)))
+
+(gen-tag-fns)
+
+;;; proxy thru to om.dom
+
+(defn render
+  [& params]
+  (apply dom/render params))
+
+(defn render-to-str
+  [& params]
+  (apply dom/render-to-str params))
+
+(defn node
+  [& params]
+  (apply dom/node params))
