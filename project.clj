72914eb5907f95bf10a3322ab1c3d3f5d1d896ab
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-08 00:22:03 +0100

    release v0.1.0

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject om-css "0.1.0-SNAPSHOT"
+(defproject com.ladderlife/om-css "0.1.0"
   :description "Om Next + CSS"
   :url "http://github.com/ladderlife/om-css"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["clojars" {:sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
-                 [org.omcljs/om "1.0.0-alpha31-SNAPSHOT"]
+                 [org.omcljs/om "1.0.0-alpha30"]
                  [garden "1.3.0"]
 
                  [figwheel-sidecar "0.5.0-4" :scope "test"]
