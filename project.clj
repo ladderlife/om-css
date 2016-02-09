70f7ddc14c9c4a4cc1189d71c1dd3ba2cdd2bd9b
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-09 17:12:08 +0100

    release 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
- (defproject com.ladderlife/om-css "0.3.0-SNAPSHOT"
+ (defproject com.ladderlife/om-css "0.3.0"
   :description "Om Next + CSS"
   :url "http://github.com/ladderlife/om-css"
   :license {:name "Eclipse Public License"
