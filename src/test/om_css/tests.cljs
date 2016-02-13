b37c022c4ce833684a3cfb72352d50e377a4fd0a
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-13 15:54:12 +0100

    add a few simple cljs tests

---
+++ src/test/om_css/tests.cljs
@@ -0,0 +1,28 @@
+(ns om-css.tests
+  (:require [cljs.test :refer-macros [deftest testing is are run-tests]]
+            [om-css.utils :as utils]))
+
+(def component-info
+  {:component-name "Foo"
+   :ns-name "ns.core"})
+
+(deftest test-format-class-name
+  (are [cn res] (= (utils/format-class-name component-info cn) res)
+       :foo "ns_core_Foo_foo"
+       "foo" "ns_core_Foo_foo")
+  (is (= (utils/format-class-name
+           {:ns-name "ns.core"
+            :component-name "ns.core/Foo"}
+           :foo)
+        "ns_core_Foo_foo")))
+
+(deftest test-format-class-names
+  (are [cns res] (= (utils/format-class-names component-info cns) res)
+    "ns_core_Foo_foo" "ns_core_Foo_foo"
+    :foo "ns_core_Foo_foo"
+    [:foo] "ns_core_Foo_foo"
+    [:foo :bar] "ns_core_Foo_foo ns_core_Foo_bar"
+    ["ns_core_Foo_foo"] "ns_core_Foo_foo"
+    ["ns_core_Foo_foo"] "ns_core_Foo_foo"
+    ["ns_core_Foo_foo" "ns_core_Foo_bar"] "ns_core_Foo_foo ns_core_Foo_bar"
+    ["ns_core_Foo_foo" :bar] "ns_core_Foo_foo ns_core_Foo_bar"))
