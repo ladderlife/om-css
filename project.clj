1d20342acdbb44f1d4db1b80ba4916843511b78c
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-19 14:10:09 +0100

    next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
- (defproject com.ladderlife/om-css "0.4.3"
+ (defproject com.ladderlife/om-css "0.4.4-SNAPSHOT"
   :description "Om Next + CSS"
   :url "http://github.com/ladderlife/om-css"
   :license {:name "Eclipse Public License"
