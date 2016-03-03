cc652f2f70d070575e644947669ba845e9d4011f
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-03 23:42:35 +0100

    fix failing cljs tests

--- src/test/om_css/tests.cljs
+++ src/test/om_css/tests.cljs
@@ -7,22 +7,22 @@
    :ns-name "ns.core"})
 
 (deftest test-format-class-name
-  (are [cn res] (= (utils/format-class-name component-info cn) res)
+  (are [cn res] (= (utils/format-class-name cn component-info) res)
     :foo "ns_core_Foo_foo"
     "foo" "ns_core_Foo_foo")
   (is (= (utils/format-class-name
+           :foo
            {:ns-name "ns.core"
-            :component-name "ns.core/Foo"}
-           :foo)
+            :component-name "ns.core/Foo"})
         "ns_core_Foo_foo")))
 
 (deftest test-format-class-names
-  (are [cns res] (= (utils/format-class-names component-info cns) res)
-    "ns_core_Foo_foo" "ns_core_Foo_foo"
-    :foo "ns_core_Foo_foo"
-    [:foo] "ns_core_Foo_foo"
-    [:foo :bar] "ns_core_Foo_foo ns_core_Foo_bar"
-    ["ns_core_Foo_foo"] "ns_core_Foo_foo"
-    ["ns_core_Foo_foo"] "ns_core_Foo_foo"
-    ["ns_core_Foo_foo" "ns_core_Foo_bar"] "ns_core_Foo_foo ns_core_Foo_bar"
-    ["ns_core_Foo_foo" :bar] "ns_core_Foo_foo ns_core_Foo_bar"))
+  (are [cns classes-seen res] (= (utils/format-class-names cns component-info classes-seen) res)
+    "ns_core_Foo_foo" #{} "ns_core_Foo_foo"
+    :foo #{:foo} "ns_core_Foo_foo"
+    [:foo] #{:foo} "ns_core_Foo_foo"
+    [:foo :bar] #{:foo :bar} "ns_core_Foo_foo ns_core_Foo_bar"
+    ["ns_core_Foo_foo"] nil "ns_core_Foo_foo"
+    ["ns_core_Foo_foo"] nil "ns_core_Foo_foo"
+    ["ns_core_Foo_foo" "ns_core_Foo_bar"] nil "ns_core_Foo_foo ns_core_Foo_bar"
+    ["ns_core_Foo_foo" :bar] #{:bar} "ns_core_Foo_foo ns_core_Foo_bar"))
