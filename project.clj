5520c859e9a908f992074dcdab884933cb488925
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-08 13:31:47 +0100

    0.4.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
- (defproject com.ladderlife/om-css "0.4.8-SNAPSHOT"
+ (defproject com.ladderlife/om-css "0.4.8"
   :description "Om Next + CSS"
   :url "http://github.com/ladderlife/om-css"
   :license {:name "Eclipse Public License"
