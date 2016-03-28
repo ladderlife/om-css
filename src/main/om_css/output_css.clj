0182c81b1365bcf077e32a58f5c43d9b1b31e3a9
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-28 22:22:31 +0200

    use the cljs analyzer api instead of poking around env/*compiler*

--- src/main/om_css/output_css.clj
+++ src/main/om_css/output_css.clj
@@ -1,26 +1,23 @@
 (ns om-css.output-css
-  (:require [cljs.env :as env]
+  (:require [cljs.analyzer.api :as ana-api]
             [clojure.java.io :as io]
             [clojure.string :as string]))
 
 (defn setup-io! []
-     (let [opts (some-> env/*compiler*
-                  deref
-                  :options)
-           default-fname "out.css"
-           fname (or (:css-output-to opts)
-                   (str (:output-dir opts) default-fname)
-                   (string/join "/"
-                     (-> (:output-to opts)
-                       (string/split #"/")
-                       butlast
-                       vec
-                       (conj default-fname))))]
-       (add-watch om-css.core/css :watcher
-         (fn [k atom old-state new-state]
-           (with-open [out ^java.io.Writer (io/make-writer fname {})]
-             (binding [*out* out]
-               (println (string/join "\n" (vals new-state)))
-               (println)))))))
+  (let [{:keys [css-output-to output-dir output-to]} (ana-api/get-options)
+        default-fname "out.css"
+        fname (or css-output-to
+                (str output-dir default-fname)
+                (string/join "/"
+                  (-> output-to
+                    (string/split  #"/")
+                    pop
+                    (conj default-fname))))]
+    (add-watch om-css.core/css :watcher
+      (fn [k atom old-state new-state]
+        (with-open [out ^java.io.Writer (io/make-writer fname {})]
+          (binding [*out* out]
+            (println (string/join "\n" (vals new-state)))
+            (println)))))))
 
 (setup-io!)
